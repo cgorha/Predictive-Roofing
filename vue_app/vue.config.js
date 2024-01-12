a7341647138d13d961e85972bf0031f1e1212639
Cole <coleman.gorham@domedia.com>
2024-01-11 20:19:27 -0500

    Architecture: Changed Vue version and setup.

--- vue_app/vue.config.js
+++ vue_app/vue.config.js
@@ -1,25 +1,4 @@
-const BundleTracker = require("webpack-bundle-tracker");
-const path = require('path');
-
-module.exports = {
-  publicPath: "http://localhost:8080/",
-  outputDir: path.resolve(__dirname, './dist/'),
-
-  chainWebpack: config => {
-
-    config.optimization.splitChunks(false);
-
-    config.plugin('BundleTracker')
-      .use(BundleTracker, [{path: path.resolve(__dirname, './dist/'), filename: 'webpack-stats.json'}]);
-
-    config.resolve.alias
-      .set('__STATIC__', 'static');
-
-    config.devServer
-      //.public('http://localhost:8080')
-      .host('localhost')
-      .port(8080)
-      .https(false)
-      .headers({"Access-Control-Allow-Origin": ["*"]});
-  }
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
