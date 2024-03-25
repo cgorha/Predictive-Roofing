b56f3f8b97c6a67171f92036622cc6aa61796815
Helen <hcardoso103@gmail.com>
2024-03-25 01:56:29 -0400

    updated message view

--- d/d/settings.py
+++ d/d/settings.py
@@ -159,3 +159,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Twilio Account Information
+TWILIO_ACCOUNT_SID = ''
+TWILIO_AUTH_TOKEN = ''
+TWILIO_PHONE_NUMBER = ''
