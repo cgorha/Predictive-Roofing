5137f1878f0bcc6bd7684cd076c528fecb170b76
Helen <hcardoso103@gmail.com>
2024-03-27 16:09:15 -0400

    Messages

--- d/project/views.py
+++ d/project/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from project.models import Lead
 from .serializers import UserSerializer, LeadSerializer
+
+import os
+from django.conf import settings
+from twilio.rest import Client
+from django.http import HttpResponse
 
 User = get_user_model()
 class UserDetailAPIView(generics.RetrieveAPIView):
@@ -34,3 +41,29 @@ class LeadDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
     permission_classes = [IsAuthenticated]
+
+def send_sms(request):
+    account_sid = settings.TWILIO_ACCOUNT_SID
+    auth_token = settings.TWILIO_AUTH_TOKEN
+    twilio_phone_number = settings.TWILIO_PHONE_NUMBER
+
+    message_body = request.POST.get('message')
+    to_phone_number = request.POST.get('to_phone_number')
+    client = Client(account_sid,auth_token)
+
+    try:
+        message = client.messages.create(
+            body=message_body,
+            from_=twilio_phone_number,
+            to=to_phone_number
+        )
+    
+        print(message.sid)
+        return HttpResponse("SMS sent successfully")
+    except Exception as e:
+        # Return error response if sending SMS fails
+        return HttpResponse(f"Error: {str(e)}", status=500)
+    else:
+        # Return error response if method is not POST
+        return HttpResponse("Only POST requests are allowed", status=405)
+    
