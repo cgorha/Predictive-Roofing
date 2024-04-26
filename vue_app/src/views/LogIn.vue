62949559a6e84bd8520f062a1760ece1804f6b3f
Helen <hcardoso103@gmail.com>
2024-04-26 02:29:28 -0400

    Updated the Login/Signup w/ new designs

--- vue_app/src/views/LogIn.vue
+++ vue_app/src/views/LogIn.vue
@@ -1,38 +1,54 @@
 <template>
     <div class="page-log-in">
-        <div class="columns">
-            <div class="column is-4 is-offset-4">
-                <h1 class="title">Log in</h1>
+        <div class="columns is-centered">
+            <div class="column is-7 box mt-6">
+                <div class="columns is-vcentered">
+                    <div class="column">
+                        <img src="../../public/log_in.png" class="image-left" alt="Sign Up Image">
+                    </div>
 
-                <form @submit.prevent="submitForm">
-                    <div class="field">
-                        <label>Username</label>
-                        <div class="control">
-                            <input type="text" class="input" v-model="username">
+                    <div class="column">
+                    
+                        <h1 class="title has-text-centered">Log In</h1>
+                        <p class="subtitle has-text-centered"> Welcome back to Predictive Roofing</p>
+                    
+
+                    <form @submit.prevent="submitForm">
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input type="text" class="input" v-model="username" placeholder="Username">
+                                <span class="icon is-small is-left">
+                                    <font-awesome-icon icon="user" />
+                                </span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="field">
-                        <label>Password</label>
-                        <div class="control">
-                            <input type="password" class="input" v-model="password">
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input type="password" class="input" v-model="password" placeholder="Password">
+                                <span class="icon is-small is-left">
+                                    <font-awesome-icon icon="key" />
+                                </span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="notification is-danger" v-if="errors.length">
-                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
-                    </div>
-
-                    <div class="field">
-                        <div class="control">
-                            <button class="button is-dark">Log in</button>
+                        <div class="notification is-danger" v-if="errors.length">
+                            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
                         </div>
+
+                        <div class="field">
+                            <div class="control">
+                                <button class="button is-fullwidth is-success">Log in</button>
+                            </div>
+                        </div>
+
+                        <hr>
+                        <div class="has-text-centered">
+                        Don't have an account? <router-link to="/sign-up">Click here</router-link> to sign up!
                     </div>
-
-                    <hr>
-
-                    Or <router-link to="/sign-up">click here</router-link> to sign up!
-                </form>
+                    </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
